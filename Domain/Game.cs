bb38f7afaf4cb1e501cefd885ede3636f8e2c7ef
igor-nikolic <nikolic.igor@outlook.com>
2019-06-14 16:09:42 +0200

    Initial commit

---
+++ Domain/Game.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain
+{
+    public class Game : BaseEntity
+    {
+        public string Name { get; set; }
+        public int Size { get; set; }
+        public string Description { get; set; }
+        public int CategoryId { get; set; }        
+        public Category Category { get; set; }
+        public int CompanyId { get; set; }
+        public Company Company { get; set; }
+        public ICollection<Rent> Rents { get; set; }
+
+    }
+}
---
+++ Domain/Rent.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain
+{
+    public class Rent : BaseEntity
+    {
+        public int UserId { get; set; }
+        public User User { get; set; }
+        public int GameId { get; set; }
+        public Game Game { get; set; }
+        public DateTime? ReturnedAt { get; set; }
+    }
+}
